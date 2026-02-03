50191633f27ee450a0ee87564ffe64d6907cfdfc
dkallattil <divia@januaryadvisors.com>
2026-02-03 14:23:27 -0500

    Updating sample data

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-# grh-demo-tutorial
-Test repo for working with Good Reason Houston on data training
+# GRH Demo Tutorial Scripts
 
-Test changes
+Cleaning scripts
 
-Divia
+-   prep_sample_data:
+
+-   join_entities:
---
+++ rscripts/join_entities.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+library(janitor)
+library(here)
+library(rio)
+
+sample_data = import(here("data-clean/sample_data.csv"))
+entities = import(here("data-raw/052025_GRH_EntitiesFile_2024-2025.csv"))
+
+#TODO Clean the variable names in the entities file and select the following: FinalDistrictName, houston MSA flag, district size
+
+#TODO Join the cleaned up entities file to the sample data
+
+#TODO Filter the entities file to just KIPP Houston or Houston ISD (you pick)
