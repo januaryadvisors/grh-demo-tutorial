62ec97995a200ab849a8e35e5544e929d1d39c99
pgill21 <p.gill21@gmail.com>
2026-02-10 14:58:04 -0600

    lez go

--- rscripts/join_entities.R
+++ rscripts/join_entities.R
@@ -8,6 +8,21 @@ entities = import(here("data-raw/052025_GRH_EntitiesFile_2024-2025.csv"))
 
 #TODO Clean the variable names in the entities file and select the following: FinalDistrictName, houston MSA flag, district size
 
-#TODO Join the cleaned up entities file to the sample data
+#TODO Join the cleaned up entities file to the sample data - PG
+
+entities <- entities %>% 
+  clean_names() %>% 
+  rename(dname = district_name)
+
+#Sample data is missing district ID so have to merge on dname
+
+#TODO Limit entities to district-level data
+
+
+
+sample_data <- sample_data %>% 
+  left_join(., entities, by = "dname")
+
+
 
 #TODO Filter the entities file to just KIPP Houston or Houston ISD (you pick)
